85aedce3a95c434b8a2f36dd96d12581ae426e60
sarah <ssheeva.sales@gmail.com>
2026-08-17 18:29:58 -0300

    linhas e colunas vazias

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 df = pd.read_csv("varejo.csv", sep=";")
-print(df.shape)
-print(df.dtypes)
-print(df.head())
+#print(df.shape)
+#print(df.dtypes)
+#print(df.head())
+
+print(df.isna().sum())
+print(df.duplicated().sum())
+print(df["PR_CAT"].value_counts(dropna=False))
