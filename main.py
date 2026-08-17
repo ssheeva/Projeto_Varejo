5659bd5d5cd5229c14155e3e8d67cbab19d11843
sarah <ssheeva.sales@gmail.com>
2026-08-17 18:38:45 -0300

    limpeza

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ print(df.shape)
 print(df.dtypes)
 print(df.head())
 
-print(df.isna().sum())
-print(df.duplicated().sum())
-print(df["PR_CAT"].value_counts(dropna=False))
+#print(df.isna().sum())
+#print(df.duplicated().sum())
+#print(df["PR_CAT"].value_counts(dropna=False))
 
-df = df.dropna(axis=1, how="all")
+#df = df.dropna(axis=1, how="all")
+
+df["PR_CAT"] = df["PR_CAT"].fillna("Sem Categoria")
+df["PR_CAT"] = df["PR_CAT"].str.strip().replace("#N/D", "Sem Categoria")
+
+df = df.drop_duplicates().copy()
