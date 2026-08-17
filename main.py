1553fe92934ef845c9737af8fc6f7b46789bde2e
sarah <ssheeva.sales@gmail.com>
2026-08-17 18:34:53 -0300

    vazio

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
 df = pd.read_csv("varejo.csv", sep=";")
-#print(df.shape)
-#print(df.dtypes)
-#print(df.head())
+print(df.shape)
+print(df.dtypes)
+print(df.head())
 
 print(df.isna().sum())
 print(df.duplicated().sum())
 print(df["PR_CAT"].value_counts(dropna=False))
+
+df = df.dropna(axis=1, how="all")
