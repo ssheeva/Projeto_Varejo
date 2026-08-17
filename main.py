d15da98858fa2bb6537ccf2c5f0eee1016199bc3
sarah <ssheeva.sales@gmail.com>
2026-08-17 18:46:44 -0300

    tabela limpa

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ print(df.shape)
 print(df.dtypes)
 print(df.head())
 
-#print(df.isna().sum())
-#print(df.duplicated().sum())
-#print(df["PR_CAT"].value_counts(dropna=False))
+print(df.isna().sum())
+print(df.duplicated().sum())
+print(df["PR_CAT"].value_counts(dropna=False))
 
-#df = df.dropna(axis=1, how="all")
+df = df.dropna(axis=1, how="all")
 
-#df["PR_CAT"] = df["PR_CAT"].fillna("Sem Categoria")
-#df["PR_CAT"] = df["PR_CAT"].str.strip().replace("#N/D", "Sem Categoria")
+df["PR_CAT"] = df["PR_CAT"].fillna("Sem Categoria")
+df["PR_CAT"] = df["PR_CAT"].str.strip().replace("#N/D", "Sem Categoria")
 
 df = df.drop_duplicates().copy()
 
 df["DATA"] = pd.to_datetime(
     df["DATA"], format="%d/%m/%Y", errors="coerce"
 )
+
+df.to_csv("varejo_limpo.csv", index=False, encoding="utf-8-sig")
+
